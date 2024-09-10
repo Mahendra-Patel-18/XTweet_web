8a617ea732bc19dd6041ba8682da8bd2186d94f1
Mahendra-Patel-18 <mahendrajay18@gmail.com>
2024-09-10 11:31:06 +0530

    code updated

--- static/script.js
+++ static/script.js
@@ -1,17 +1,14 @@
-// script.js
-
 document.addEventListener("DOMContentLoaded", function () {
     const tweetTexts = document.querySelectorAll(".xtweet-text");
 
     tweetTexts.forEach(function (textElement) {
         const fullText = textElement.innerText.trim();
-        const maxLength = 200; // Set maximum character length for preview
+        const maxLength = 200; 
 
         if (fullText.length > maxLength) {
             const shortText = fullText.substring(0, maxLength) + "...";
             textElement.innerText = shortText;
 
-            // Create Read More/Read Less link
             const readMoreLink = document.createElement("a");
             readMoreLink.href = "#";
             readMoreLink.className = "read-more-less";
@@ -19,7 +16,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
             textElement.parentNode.appendChild(readMoreLink);
 
-            // Event listener to toggle text
             readMoreLink.addEventListener("click", function (e) {
                 e.preventDefault();
                 if (readMoreLink.innerText === "Read More") {
@@ -39,15 +35,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const searchForm = document.getElementById('searchForm');
     const searchButton = document.getElementById('searchButton');
 
-    // Disable the search button initially
     searchButton.disabled = true;
 
-    // Listen for input changes
     searchInput.addEventListener('input', function() {
         if (searchInput.value.length >= 4) {
-            searchButton.disabled = false;  // Enable the button if input is 5 or more characters
+            searchButton.disabled = false;
         } else {
-            searchButton.disabled = true;  // Disable the button otherwise
+            searchButton.disabled = true;
         }
     });
 });
