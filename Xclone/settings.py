8d12bcc3e6a1e7a5803d80f4de492b7983c740fb
Mahendra-Patel-18 <mahendrajay18@gmail.com>
2024-09-20 11:31:06 +0530

    Update settings for production

--- Xclone/settings.py
+++ Xclone/settings.py
@@ -23,13 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)hyzi*!u#$^@=cid0-cyi93_bqw0cm!)16(2_$7#%^j=b4%*wn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True  # Change to True temporarily for debugging
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
-
+ALLOWED_HOSTS = ['127.0.0.1', 'webxtweet18.vercel.app']  # Replace with your actual Vercel domain
 
 # Application definition
-
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.admin',
@@ -72,10 +70,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Xclone.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -83,11 +79,8 @@ DATABASES = {
     }
 }
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -103,27 +96,19 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
@@ -135,8 +120,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Directories where Django will look for static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-
 LOGIN_URL = '/accounts/login'
-
 LOGIN_REDIRECT_URL = '/X/'
 LOGOUT_REDIRECT_URL = '/X/'
